8c41d73967e96ce04e138e99072d286c90da8ae9
igor-zalevskii <igor-zalevskii@yandex.ru>
2025-06-02 17:47:28 +0300

    Create 001.c

---
+++ 001/001.c
@@ -0,0 +1,18 @@
+/*
+Sum of all the multiples of 3 or 5 bellow 1000.
+Brute-force: O(n).
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    int ans = 0;
+
+    for (int i = 1; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            ans += i;
+        }
+    }
+    printf("%i\n", ans);
+    return 0;
+}
